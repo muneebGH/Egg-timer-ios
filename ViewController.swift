bbcea8006f383cc3c18503be011ecaac89b9b7f6
muneebGH <muneeb.github@gmail.com>
2020-02-12 14:47:01 +0500

    completed all requirments

---
+++ ViewController.swift
@@ -0,0 +1,51 @@
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+
+    var timer:Timer?
+    var totalTime:Float=0.0
+    var startTime:Float=0.0
+    let eggTimes=["Soft":300 , "Medium":420 , "Hard":720]
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        
+        progressView.progress=0
+        startTime=0.0
+      
+        label.text="How do you like your eggs?"
+        timer?.invalidate()
+        
+        totalTime=Float(eggTimes[sender.currentTitle!]!)
+        startTimer()
+        
+    }
+    
+    func startTimer(){
+        timer=Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func updateTime(){
+       
+        
+        
+        startTime+=1
+        
+        progressView.progress=startTime/totalTime
+        
+        if startTime==totalTime{
+            timer?.invalidate()
+            label.text="Done!"
+        }
+        
+    }
+    
+    
+
+}
